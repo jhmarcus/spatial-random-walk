c12d78f3c8e59c3ddf30e697d6ff1eda476cdb15
jhmarcus <jhmarcus@uchicago.edu>
2018-01-11 14:17:59 -0600

    removed name from env

--- .sos/default_2_0_14b7ecc1.sh
+++
@@ -1,1 +0,0 @@
-rm -f analysis/_index.ipynb setup/_index.ipynb license/_index.ipynb notes/_index.ipynb
--- .sos/setup-jnbinder_0_0_a146dfca.sh
+++
@@ -1,4 +0,0 @@
-yes | cp -rf jnbinder-master/docs ./
-cp jnbinder-master/config.yml config.default.yml
-rm -rf jnbinder-master*
-
