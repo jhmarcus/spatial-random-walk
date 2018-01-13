d6ce246926a44cbc90aa88fa96f3d64b8dec94e0
Joseph Marcus <jhmarcus@uchicago.edu>
2018-01-13 12:13:14 -0600

    starting habitat module

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 [![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/jhmarcus/spatial-random-walk/master)
 
-This is an *in progress* set of explorations for applying spatial random walk
+This is an *in progress* set of explorations of applying spatial random walk
 models to genetic data. Click the above [mybinder](https://mybinder.org/) button
 to run some of the analyses and explore the ideas yourself.
---
+++ code/habitat.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import networkx as nx
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import cm
+
+
+class Habitat(object):
+    """Class for defining, visualzing and computing 
+    on a habitat which is a directed graph with 
+    a set of specified edge weights
+    """
+    def __init__(self, d):
+        # number of nodes in the graph 
+        self.d = d 
+        self.
+    
+    def compute_resistance_distance(self, m):
+        """
+        """
+        pass
+
+
+class TriangularLattice(Habitat):
+    """
+    """
+    pass
+
+
+class Circle(Habitat):
+    """
+    """
+    pass
