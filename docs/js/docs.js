d8aa29a7ff74fe1cf1cfc36aaa70cb0da0cad150
jhmarcus <jhmarcus@uchicago.edu>
2018-01-18 08:06:27 -0600

    added meeting notes

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
 var analysisArray = ['spatial-autoregressive', 'laplacian-smoothing', 'trilat-bar', 'circ-unif', 'trilat-asym']
-var analysisDict = {"Circular-habitat-uniform-1":"circ-unif","Understanding-laplacian-smoothing-1":"laplacian-smoothing","Understanding-autoregressive-models-1":"spatial-autoregressive","Triangular-lattice-asymmetric-1":"trilat-asym","Triangular-lattice-barrier-1":"trilat-bar"}
+var analysisDict = {"Circular-habitat-uniform-1":"circ-unif","Understanding-laplacian-smoothing-1":"laplacian-smoothing","Meetings-1":"meetings","Understanding-autoregressive-models-1":"spatial-autoregressive","Triangular-lattice-asymmetric-1":"trilat-asym","Triangular-lattice-barrier-1":"trilat-bar"}
 var licenseArray = []
 var licenseDict = {}
